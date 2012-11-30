cfb39956bb0cf2ba10925936cf19f68a786d7163
mizu-tama <tamaki.mizuno@gmail.com>
2012-11-30 22:08:57 +0900

    サイト上で編集できるの知らんかった。

--- src/FunUtils.java
+++ src/FunUtils.java
@@ -4,22 +4,32 @@ import java.util.List;
 public class FunUtils {
 
 	public static <T> InfinityList<T> cycle(List<T> xs) {
-		// TODO Auto-generated method stub
 		return new InfinityList<T>(xs);
 	}
 
-	public static <T> List<T> take(int num, InfinityList<T> xs) {
-		// TODO Auto-generated method stub
-		return null;
+	public static <T> List<T> take(int num, InfinityList<T> iList) {
+		List<T> result = new ArrayList(num);
+		
+		// TODO なんかループしてresultにつめる
+		
+		
+		return result;
 	}
 
 	public static <T, R> List<R> map(Fun<T, R> f, List<T> xs) {
-		// TODO Auto-generated method stub
-		return null;
+		List<R> result = new ArrayList(xs.size());
+		
+		for (T x: xs) {
+			result.add(f._(x));
+		}
+		
+		return result;
 	}
 
 	public static <T1, T2, R> List<R> zipWith(Fun<T1, Fun<T2, R>> f, List<T1> xs, List<T2> ys) {
-		// TODO Auto-generated method stub
+		// TODO 短い方のリストに合わせる
+		
+		
 		return null;
 	}
 }
