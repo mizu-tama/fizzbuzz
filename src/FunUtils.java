63f328bf622c0d220ec262e67b1ac03a41085df1
mizu-tama <tamaki.mizuno@gmail.com>
2012-12-13 16:39:56 +0900

    zipWith,cycle,takeを実装

--- src/FunUtils.java
+++ src/FunUtils.java
@@ -1,35 +1,45 @@
-import java.util.List;
-
+import java.util.*;
 
 public class FunUtils {
 
-	public static <T> InfinityList<T> cycle(List<T> xs) {
-		return new InfinityList<T>(xs);
-	}
+    public static <T> InfinityList<T> cycle(List<T> xs) {
+        return new InfinityList<T>(xs);
+    }
 
-	public static <T> List<T> take(int num, InfinityList<T> iList) {
-		List<T> result = new ArrayList(num);
-		
-		// TODO なんかループしてresultにつめる
-		
-		
-		return result;
-	}
+    public static <T> List<T> take(int num, InfinityList<T> iList) {
+        List<T> result = new ArrayList<T>(num);
 
-	public static <T, R> List<R> map(Fun<T, R> f, List<T> xs) {
-		List<R> result = new ArrayList(xs.size());
-		
-		for (T x: xs) {
-			result.add(f._(x));
-		}
-		
-		return result;
-	}
+        for (int i = 0; i < num; i++) {
+            result.add(iList.next());
+        }
 
-	public static <T1, T2, R> List<R> zipWith(Fun<T1, Fun<T2, R>> f, List<T1> xs, List<T2> ys) {
-		// TODO 短い方のリストに合わせる
-		
-		
-		return null;
-	}
+        return result;
+    }
+
+    public static <T, R> List<R> map(Fun<T, R> f, List<T> xs) {
+        List<R> result = new ArrayList<R>(xs.size());
+
+        for (T x : xs) {
+            result.add(f._(x));
+        }
+
+        return result;
+    }
+
+    public static <T1, T2, R> List<R> zipWith(Fun<T1, Fun<T2, R>> f, List<T1> xs, List<T2> ys) {
+        int minListSize = min(xs.size(), ys.size());
+        List<R> result = new ArrayList<R>(minListSize);
+
+        for (int i = 0; i < minListSize; i++) {
+            T1 x = xs.get(i);
+            T2 y = ys.get(i);
+            result.add(f._(x)._(y));
+        }
+
+        return result;
+    }
+
+    private static int min(int val1, int val2) {
+        return val1 < val2 ? val1 : val2;
+    }
 }
